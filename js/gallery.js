1a1777c81657949a2f60e7955c8855bc6010e40d
Ying_Y <ziyingliu@Ivyyyys-MBP.home>
2021-07-23 23:13:02 -0700

    updates

--- js/gallery.js
+++ js/gallery.js
@@ -8,10 +8,10 @@ var captions = [];
 var captionName = [];
 var openCaptionTag = "<caption class='caption'>";
 var closeCaptionTag = "</caption>";
-var description=[];
+var description;
 var descText = "Adopt Me Now";
 var openDescTag = "<button id='myBtn'>";
-var closeDescTag = "</button>"
+var closeDescTag = "</button>";
 
 //create a loop to create 10 images starting with index of 0
 for (var i=0; i<10; i++) {
@@ -19,7 +19,7 @@ for (var i=0; i<10; i++) {
     captionName.push("License number:A000" + (i+1));
     photos.push("<img src='images/" + fileNames[i] + ".jpeg'/>");
     captions.push(openCaptionTag + "<h3>"+ captionName[i] + "</h3>" + closeCaptionTag);
-    description.push(openDescTag + descText +closeDescTag);
+    description=openDescTag + descText +closeDescTag;
     image = openList + photos[i] + captions[i] + description + closeList;
     imageList.push(image);
 
